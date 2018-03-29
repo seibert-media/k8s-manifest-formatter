0ea3167abb747e7a40e74ee316876c4844aedca4
Benjamin Borbe <bborbe@rocketnews.de>
2018-03-29 19:39:01 +0200

    add test

--- k8s_manifest_formatter_test.go
+++ k8s_manifest_formatter_test.go
@@ -2,9 +2,36 @@ package main
 
 import "testing"
 
-func TestFormatYaml(t *testing.T) {
+func TestFormatYamlIlllega(t *testing.T) {
 	_, err := formatYaml([]byte(`illegal content`))
 	if err == nil {
 		t.Fatal("err expected")
+	}
+}
+
+func TestFormatYaml(t *testing.T) {
+	content:=`apiVersion: extensions/v1beta1
+kind: Ingress
+metadata:
+  annotations:
+    kubernetes.io/ingress.class: traefik
+    traefik.frontend.priority: '10000'
+  labels:
+    app: webdav
+  name: webdav
+  namespace: webdav
+spec:
+  rules:
+  - host: webdav.benjamin-borbe.de
+    http:
+      paths:
+      - backend:
+          serviceName: webdav
+          servicePort: web
+        path: /
+`
+	_, err := formatYaml([]byte(content))
+	if err != nil {
+		t.Fatal("err not expected")
 	}
 }
